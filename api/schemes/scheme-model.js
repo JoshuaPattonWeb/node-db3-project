9fba8ffd56970ff0d4f3424153d186d9e46753cd
Joshua Patton <joshuapattonweb@gmail.com>
2022-02-16 19:12:15 -0500

    middleware completed. doing some of the models

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,4 +1,6 @@
-function find() { // EXERCISE A
+const db = require("../../data/db-config")
+
+async function find() { // EXERCISE A
   /*
     1A- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`.
     What happens if we change from a LEFT join to an INNER join?
@@ -15,9 +17,16 @@ function find() { // EXERCISE A
     2A- When you have a grasp on the query go ahead and build it in Knex.
     Return from this function the resulting dataset.
   */
+ const schemeRow = await db('schemes as sc')
+    .leftJoin('steps at st', 'sc.scheme_id', 'st.scheme_id')
+    .groupBy('sc.scheme.id')
+    .select('sc.*')
+    .count('st.step_id as number_of_steps')
+
+    return schemeRow;
 }
 
-function findById(scheme_id) { // EXERCISE B
+async function findById(scheme_id) { // EXERCISE B
   /*
     1B- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`:
 
@@ -83,6 +92,8 @@ function findById(scheme_id) { // EXERCISE B
         "steps": []
       }
   */
+      const schemeRow = await db('schemes as sc')
+      .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
 }
 
 function findSteps(scheme_id) { // EXERCISE C
